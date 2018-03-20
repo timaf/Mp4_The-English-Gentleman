aa2e1c73cf2420ab485458b11f564935e0a4b1da
timaf <lo_sa_na@yahoo.com>
2018-03-20 19:46:12 +0100

    first step

---
+++ src/main/java/at/refugeescode/Mp4_TheEnglishGentleman/Mp4TheEnglishGentlemanApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.Mp4_TheEnglishGentleman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp4TheEnglishGentlemanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mp4TheEnglishGentlemanApplication.class, args);
+	}
+}
